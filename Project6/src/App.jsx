405472d7ec0cd10bf0c7f93afcb4b932652c99ad
Biswayan <biswayanpaul2015@gmail.com>
2025-04-18 22:23:33 +0530

    6th Project Hidden Search Bar

---
+++ Project6/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HiddenSearchbar from './Components/HiddenSearchbar'
+
+const App = () => {
+  return (
+    <div>
+      <HiddenSearchbar />
+    </div>
+  )
+}
+
+export default App
